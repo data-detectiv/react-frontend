bf50deb87b90461adc4e2c4371b558c60884e44e
Foster Oppong <oppongyaw049@gmail.com>
2025-06-19 22:15:19 -0400

    color scheme react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import SearchColor from "./SearchColor";
+import Square from "./Square";
 
 function App() {
+  const [colorValue, setColorValue] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square
+        colorValue={colorValue}
+      />
+      <SearchColor 
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+      />
     </div>
   );
 }
